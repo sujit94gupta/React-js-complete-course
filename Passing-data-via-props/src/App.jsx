6c56d2352d5931f0ca60f6a6912c63b2efc6b26e
Sujit Gupta <Sujit94gupta@gmail.com>
2025-07-24 20:06:05 +0530

    Intial Commit

--- Passing-data-via-props/src/App.jsx
+++ Passing-data-via-props/src/App.jsx
@@ -5,31 +5,73 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const info = [{ name: "Sujit Gupta", age: 25 }, { name: "John Doe", age: 30 }];
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Passing Data via Props</h1>
+      <h3>Pass Data Basic and Array</h3>
+      <Student name={"Sujit Gupta"} age={25} info={info} />
+      <br />
+      <h3>Pass Destructring Props</h3>
+      <Student2 name={"John Doe"} age={30} info={info} />
+      <br />
+      <h3>Function as Props (Advance)</h3>
+      <Student3 handleClick={() => alert("Clicked")} />
+      <br />
+      <h3>Looping and Passing Props</h3>
+      {info.map((student, index) => (
+        <Student4 key={index} name={student.name} age={student.age} />
+      ))}
     </>
   )
 }
 
 export default App
+
+
+function Student(props) {
+  return (
+    <>
+      <p>Student Name: {props.name}</p>
+      <p>Student Age: {props.age}</p>
+      {props.info.map((student, index) => (
+        <div key={index}>
+          <p>Name: {student.name}</p>
+          <p>Age: {student.age}</p>
+        </div>
+      ))}
+    </>
+  );
+}
+
+
+function Student2({ name, age, info }) {
+  return (
+    <>
+      <p>Student Name: {name}</p>
+      <p>Student Age: {age}</p>
+      {info.map((student, index) => (
+        <div key={index}>
+          <p>Name: {student.name}</p>
+          <p>Age: {student.age}</p>
+        </div>
+      ))}
+    </>
+  );
+}
+
+function Student3({ handleClick }) {
+  return <button onClick={handleClick}>Click Me</button>;
+}
+
+
+
+function Student4({ name, age }) {
+  return (
+    <>
+      <p>Student Name: {name}</p>
+      <p>Student Age: {age}</p>
+    </>
+  );
+}
